d1883fa7ee89e14db38debebde51c325488646a9
Jeff Heller <jeffheller@jhgfd.com>
2023-12-17 16:57:12 -0500

    Improved import process, removed test files from script.

--- Visualizer-Tools/main.py
+++ Visualizer-Tools/main.py
@@ -1,22 +1,30 @@
 # main.py
 
-from Visualizer-Tools import import_midi, clean_data, add_sections, note_lengths, generate_fake_notes, create_keyboard, keyboard_side, BPMtoFPS
+import os
+import argparse
+from . import (import_midi, clean_data, add_sections, note_lengths, generate_fake_notes, create_keyboard,
+               keyboard_side, BPMtoFPS)
 
-def main():
-    df = import_midi([
-        'CSVs/Six Marimbas Track 1.csv', 
-        'CSVs/Six Marimbas Track 2.csv', 
-        'CSVs/Six Marimbas Track 3.csv', 
-        'CSVs/Six Marimbas Track 4.csv', 
-        'CSVs/Six Marimbas Track 5.csv', 
-        'CSVs/Six Marimbas Track 6.csv'
-    ])
-    df = clean_data(df)
+
+def main(file_list):
+    # Check that the files exist at the specified locations
+    for file in file_list:
+        if not os.path.isfile(file):
+            raise FileNotFoundError(f"No file found at the specified location: {file}")
+    df = import_midi(file_list)
+    df, division = clean_data(df)
     df = add_sections(df, [1, 329, 676])
     df = note_lengths(df)
     fake_notes = list(generate_fake_notes())
     keyboard = create_keyboard(df, fake_notes)
     df = keyboard_side(df, keyboard)
+    df = BPMtoFPS(df)
+    df.to_csv('output.csv', index=False)
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Process some files.")
+    parser.add_argument("files", nargs="+", help="List of files to process")
+    args = parser.parse_args()
+
+    main(args.files)
