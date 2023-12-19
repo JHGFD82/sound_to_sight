87bda195d8bfe20c8ddd712536af54adc9e50c75
Jeff Heller <jeffheller@jhgfd.com>
2023-12-19 02:59:37 -0500

    Refactored to include better docstring and error catching.

--- Visualizer-Tools/clean_data.py
+++ Visualizer-Tools/clean_data.py
@@ -1,15 +1,48 @@
-# clean_data
 import pandas as pd
 
 
-def clean_data(df, quantization):
-    '''
-    Process the DataFrame resulting from the import_midi module.
-    '''
+def clean_data(df):
+    """
 
-    for column in ['time', 'num', 'note', 'velocity', 'ex1', 'ex2', 'player']:
-        df[column] = pd.to_numeric(df[column], errors='coerce').astype('Int32')
+    Cleans and processes the given DataFrame.
 
-    df['time'] = (df['time'] / quantization + 1).astype('Int32')  # simplify time, starting at 1
-    df = df[['time', 'status', 'note', 'velocity', 'player']].iloc[9:-2, :]
-    return df
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the data to be cleaned and processed.
+
+    Returns:
+        tuple: A tuple containing the cleaned DataFrame, the division value extracted from the DataFrame,
+            and the notes per bar value extracted from the DataFrame.
+
+    Raises:
+        ValueError: If the DataFrame is missing a 'Header' row or a 'Time_signature' row.
+
+    """
+    def convert_columns_to_numbers(dataframe):
+        for column in ['time', 'num', 'note', 'velocity', 'extra1', 'extra2', 'player']:
+            dataframe[column] = pd.to_numeric(dataframe[column], errors='coerce').astype('Int32')
+        dataframe['status'] = dataframe['status'].str.strip()
+        return dataframe
+
+    def extract_relevant_data(dataframe):
+        try:
+            division_extract = dataframe[dataframe['status'] == "Header"]['velocity'].values[0]
+        except IndexError:
+            raise ValueError("The DataFrame is missing a 'Header' row!")
+
+        try:
+            notes_per_bar_extract = dataframe[dataframe['status'] == "Time_signature"]['num'].values[0]
+        except IndexError:
+            raise ValueError("The DataFrame is missing a 'Time_signature' row!")
+
+        valid_statuses = ['Note_on_c', 'Note_off_c']
+        dataframe_filtered = dataframe[dataframe['status'].isin(valid_statuses)][
+            ['time', 'status', 'note', 'velocity', 'player']]
+
+        dataframe_filtered = dataframe_filtered.reset_index(drop=True)
+
+        return dataframe_filtered, division_extract, notes_per_bar_extract
+
+    df = convert_columns_to_numbers(df)
+    df, division, notes_per_bar = extract_relevant_data(df)
+
+    return df, division, notes_per_bar
