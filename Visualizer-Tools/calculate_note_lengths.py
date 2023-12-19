ccff779682018cb8f44441fbb1ed78428ef10e11
Jeff Heller <jeffheller@jhgfd.com>
2023-12-19 03:01:09 -0500

    Refactored for better organization, docstring, and naming conventions.

--- Visualizer-Tools/calculate_note_lengths.py
+++ Visualizer-Tools/calculate_note_lengths.py
@@ -6,11 +6,39 @@ NOTE_OFF_STATUS = 'Note_off_c'
 
 
 def calculate_note_lengths(df):
+    """
+    This method calculates the lengths of notes in a dataframe.
+
+    Parameters:
+    - df: pandas DataFrame
+        The dataframe containing MIDI note data. It should have the following columns:
+        - 'time': float
+            The time at which the MIDI event occurred.
+        - 'status': int
+            The status code of the MIDI event.
+        - 'note': int
+            The MIDI note number.
+
+    Returns:
+    - df: pandas DataFrame
+        The modified dataframe with an additional column 'length' containing the lengths of the notes.
+
+    Example usage:
+        df = pd.DataFrame({
+            'time': [0.1, 0.3, 0.5, 0.8],
+            'status': [NOTE_ON_STATUS, NOTE_ON_STATUS, NOTE_OFF_STATUS, NOTE_ON_STATUS],
+            'note': [60, 62, 60, 64]
+        })
+        df = calculate_note_lengths(df)
+
+    Note: This method assumes that NOTE_ON_STATUS and NOTE_OFF_STATUS are defined as constants.
+
+    """
     note_start_times = {}
     df['length'] = pd.Series([0] * len(df), dtype='Int32')
 
     def process_note(row):
-        time, status, note = row[1], row[2], row[3]
+        time, status, note = row[0], row[1], row[2]
         if status == NOTE_ON_STATUS:
             note_start_times[note] = (time, row.name)
         elif status == NOTE_OFF_STATUS:
