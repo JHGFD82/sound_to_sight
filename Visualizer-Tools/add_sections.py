4962c92fb03199f1a0d42eda9f19e90ed42a0dcc
Jeff Heller <jeffheller@jhgfd.com>
2023-12-19 03:00:22 -0500

    Refactored for better docstring and variable names, amongst other enhancements.

--- Visualizer-Tools/add_sections.py
+++ Visualizer-Tools/add_sections.py
@@ -1,21 +1,32 @@
-#add_sections.py
+def add_sections(dataframe, section_bar_positions, bar_division, notes_per_bar):
+    """
+    Adds a 'section' column to the given dataframe based on the provided section bar positions, bar division, and notes
+    per bar.
 
-def add_sections(df, sections):
-    '''
+    Args:
+        dataframe (pandas.DataFrame): The dataframe to which the 'section' column is added.
+        section_bar_positions (list): A list of section bar positions.
+        bar_division (int): The number of divisions in a bar.
+        notes_per_bar (int): The number of notes in a bar.
 
+    Returns:
+        pandas.DataFrame: The dataframe with the 'section' column added.
+    """
+    if section_bar_positions:
+        sections_time = [((section - 1) * bar_division * notes_per_bar) for section in
+                         ([1] + section_bar_positions if section_bar_positions[0] != 1 else section_bar_positions)]
+    else:
+        sections_time = [1]
 
+    def determine_section(music_time_position):
+        section = 1  # If sections_time is empty, return section 1
+        for index, start_time in enumerate(sections_time):
+            if index + 1 < len(sections_time) and music_time_position < sections_time[index + 1]:
+                return section
+            section += 1
+        return section if music_time_position < sections_time[-1] else section - 1
 
-    '''
-    # Add the starting position for the first section
+    music_positions = dataframe['time']
+    dataframe['section'] = music_positions.apply(determine_section).astype('Int32')
 
-    sections = [(x * 8 - 7) for x in ([1] + sections if sections[0] != 1 else sections)]
-
-    # Function to determine the section based on position
-    def find_section(position):
-        for i, start in enumerate(sections):
-            if position < sections[i + 1] if i + 1 < len(sections) else True:
-                return i + 1
-
-    # Apply the function to each row in the DataFrame
-    df['section'] = df['time'].apply(find_section).astype('Int32')
-    return df
+    return dataframe
