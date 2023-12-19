d0b2234eab765f58ecd7d2e997ea2fc444b19324
Jeff Heller <jeffheller@jhgfd.com>
2023-12-19 18:46:00 -0500

    Added additional setup parameters, fleshed out classifiers.

--- Visualizer-Tools/setup.py
+++ Visualizer-Tools/setup.py
@@ -3,11 +3,28 @@ from setuptools import setup, find_packages
 setup(
     name='Visualizer Tools',
     version='0.1.0',
+    author='Jeff Heller (JHGFD)',
+    author_email='jeffheller@jhgfd.com',
     packages=find_packages(),
+    install_requires=['pandas'],
     entry_points={
         'console_scripts': [
             'Visualizer-Tools = Visualizer-Tools.main:main',
         ],
     },
-    # ... other setup parameters ...
+    url="https://github.com/JHGFD82/Visualizer-Tools",
+    description="Tools to assist in the process of turning music data into video projects.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Artistic Software",
+        "Topic :: Multimedia :: Sound/Audio :: MIDI",
+        "Topic :: Multimedia :: Video"
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
 )
