86de3c6bcb7b3016b80e0efffb8047d118103ca8
Jeff Heller <jeffheller@jhgfd.com>
2023-12-28 09:58:08 -0500

    Refactoring for readability.

--- sound_to_sight/create_jsons.py
+++ sound_to_sight/create_jsons.py
@@ -19,19 +19,17 @@ def identify_and_hash_patterns(df, notes_per_bar, division):
     pattern_details = []
     ticks_per_pattern = notes_per_bar * division
 
-    players = df['player'].unique()
-    players = [int(x) for x in players]
-    for player in players:
+    for player in df['player'].unique():
         df_player = df[df['player'] == player]
 
-        for start_tick in range(0, max(df_player['time']), ticks_per_pattern):
+        max_time = df_player['time'].max()
+        for start_tick in range(0, max_time, ticks_per_pattern):
             end_tick = start_tick + ticks_per_pattern
-            pattern_df = df_player[(df_player['time'] >= start_tick) & (df_player['time'] < end_tick)].copy()
-            pattern_df[['note', 'velocity', 'length']] = pattern_df[['note', 'velocity', 'length']].astype(int)
+            pattern_df = df_player[(df_player['time'] >= start_tick) & (df_player['time'] < end_tick)]
             pattern = convert_pattern_to_hashable(pattern_df)
             pattern_hash = hash_pattern(pattern)
 
-            pattern_key = (player, pattern_hash)  # Unique key for each player-pattern combination
+            pattern_key = (int(player), pattern_hash)
 
             if pattern_key not in patterns:
                 patterns[pattern_key] = pattern
@@ -47,28 +45,26 @@ def create_pattern_timing_json(df, patterns, notes_per_bar, division):
     pattern_timing_data = []
     ticks_per_pattern = notes_per_bar * division
 
-    players = df['player'].unique()
-    for player in players:
+    pattern_hashes = {hash_pattern(pattern): pattern for pattern in patterns.values()}
+
+    for player in df['player'].unique():
         df_player = df[df['player'] == player]
 
-        for pattern_hash, pattern in patterns.items():
-            for start_tick in range(0, max(df_player['time']), ticks_per_pattern):
-                end_tick = start_tick + ticks_per_pattern
-                pattern_df = df_player[(df_player['time'] >= start_tick) & (df_player['time'] < end_tick)]
+        max_time = df_player['time'].max()
+        for start_tick in tqdm(range(0, max_time, ticks_per_pattern)):
+            end_tick = start_tick + ticks_per_pattern
+            pattern_df = df_player[(df_player['time'] >= start_tick) & (df_player['time'] < end_tick)]
+            segment_pattern = convert_pattern_to_hashable(pattern_df)
+            segment_hash = hash_pattern(segment_pattern)
 
-                if is_matching_pattern(pattern_df, pattern):
-                    pattern_timing_data.append({
-                        "pattern_hash": pattern_hash,
-                        "start_time": int(start_tick),
-                        "player": int(player)
-                    })
+            if segment_hash in pattern_hashes:
+                pattern_timing_data.append({
+                    "pattern_hash": segment_hash,
+                    "start_time": start_tick,
+                    "player": int(player)
+                })
 
     return {"pattern_timings": pattern_timing_data}
-
-
-def is_matching_pattern(segment_df, pattern):
-    segment_pattern = convert_pattern_to_hashable(segment_df)
-    return hash_pattern(segment_pattern) == hash_pattern(pattern)
 
 
 def create_jsons(df, notes_per_bar, division):
@@ -78,5 +74,5 @@ def create_jsons(df, notes_per_bar, division):
     # Writing JSON files
     with open('pattern_details.json', 'w') as file:
         json.dump(pattern_details, file)
-    with open('pattern_timings.json', 'w') as file:
+    with open('pattern_timing_json.json', 'w') as file:
         json.dump(pattern_timing_json, file)
