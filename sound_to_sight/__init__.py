894b003af1fc4219f2f8294d160e33d30cfe8650
Jeff Heller <jeffheller@jhgfd.com>
2023-12-28 20:04:58 -0500

    Added add_measure_time to __init__.

--- sound_to_sight/__init__.py
+++ sound_to_sight/__init__.py
@@ -3,6 +3,7 @@ from .import_midi import import_midi
 from .clean_data import clean_data
 from .add_sections import add_sections
 from .calculate_note_lengths import calculate_note_lengths
+from .calculate_note_lengths import add_measure_time
 from .assign_instrument_json import assign_instrument_json
 from .extract_positions import extract_positions
 from .add_timings import add_timings
