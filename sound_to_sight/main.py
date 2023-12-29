b3990d6cef7230d7f91715e3b19e8f818c06bfe2
Jeff Heller <jeffheller@jhgfd.com>
2023-12-28 20:04:44 -0500

    Change main script to allow in-script testing.

--- sound_to_sight/main.py
+++ sound_to_sight/main.py
@@ -30,37 +30,47 @@ def main(file_list, bpm, fps, tpb, sections=None, action_safe=False):
     Returns:
         None
     """
+
+    # FILE IMPORT
     for file in file_list:
         if not os.path.isfile(file):
             raise FileNotFoundError(
                 f'Failed to open file "{file}". Please ensure that the file exists and that you have entered the '
                 f'correct file name.')
     df = import_midi(file_list)
+
+    # CLEAN DATA
     df, division, notes_per_bar = clean_data(df)
+
+    # ADD SECTIONS
     if sections is None:
         print("No sections provided via command-line arguments.")
         sections = (input("If the music has sections you want to designate, enter their bar numbers here separated by "
                           "spaces, or simply hit enter to continue: ").split())
-    df = add_sections(df, sections, division, notes_per_bar)
+    df = add_sections(df, division, notes_per_bar, sections)
+
+    # MEASURE ANALYSIS
     df = calculate_note_lengths(df)
-    layouts = assign_instrument_json(df)
-    df = extract_positions(df, layouts)
-    df = add_timings(df, bpm, fps, tpb)
-    df.to_csv('output.csv', index=False)
     df = add_measure_time(df, division, notes_per_bar)
     print(df)
+    # layouts = assign_instrument_json(df)
+    # df = extract_positions(df, layouts)
+    # df = add_timings(df, bpm, fps, tpb)
+    # df.to_csv('output.csv', index=False)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process some files.")
-    parser.add_argument("-i", "--input_files", nargs="+", help="List of files to process.")
-    parser.add_argument("-s", "--sections", nargs="+", default=None, help="List of sections (if any).")
-    parser.add_argument("-a", "--action_safe", action="store_true",
-                        help="Boolean to accommodate action safe zones in video pixel resolution")
-    parser.add_argument('-b', '--bpm', type=float, required=True, help='Beats per minute of the song')
-    parser.add_argument('-f', '--fps', type=float, required=True, help='Frames per second of the video')
-    parser.add_argument('-tpb', '--ticks_per_beat', default=TPB, type=int,
-                        help='Number of ticks per beat (default is 480)')
-    args = parser.parse_args()
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description="Process some files.")
+#     parser.add_argument("-i", "--input_files", nargs="+", help="List of files to process.")
+#     parser.add_argument("-s", "--sections", nargs="+", default=None, help="List of sections (if any).")
+#     parser.add_argument("-a", "--action_safe", action="store_true",
+#                         help="Boolean to accommodate action safe zones in video pixel resolution")
+#     parser.add_argument('-b', '--bpm', type=float, required=True, help='Beats per minute of the song')
+#     parser.add_argument('-f', '--fps', type=float, required=True, help='Frames per second of the video')
+#     parser.add_argument('-tpb', '--ticks_per_beat', default=TPB, type=int,
+#                         help='Number of ticks per beat (default is 480)')
+#     args = parser.parse_args()
+#
+#     main(args.input_files, args.bpm, args.fps, args.ticks_per_beat, args.sections, args.action_safe)
 
-    main(args.input_files, args.bpm, args.fps, args.ticks_per_beat, args.sections, args.action_safe)
+main(['../../Six Marimbas/Music/Six.csv'], 192, 29.97, TPB, sections=[329,676])
