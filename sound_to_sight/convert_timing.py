feb207850d2281a2bcf88835d8af320870072f24
Jeff Heller <jeffheller@jhgfd.com>
2023-12-30 03:18:28 -0500

    Convert_timing updated to replace time values in details and timing dictionaries.

--- sound_to_sight/convert_timing.py
+++ sound_to_sight/convert_timing.py
@@ -1,8 +1,35 @@
-from BPMtoFPS import ticks_to_seconds, seconds_to_timecode, seconds_to_frames
+from BPMtoFPS import ticks_to_seconds, seconds_to_frames
 
 
-def add_timings(df, division, bpm, fps):
-    seconds = df['time'].apply(lambda x: ticks_to_seconds(x, bpm, division))
-    df['frames'] = seconds.apply(lambda x: seconds_to_frames(x, fps))
-    df['timecode'] = seconds.apply(lambda x: seconds_to_timecode(x, fps))
-    return df
+def convert_timing(details, timing, division, bpm, fps):
+    # Convert ticks to frames for the 'details' dictionary
+    for instrument, instrument_patterns in details.items():
+        for pattern in instrument_patterns:
+            for i, note in enumerate(pattern['notes']):
+                note = list(note)
+                timing_ticks = note[0]  # Get the timing information
+                duration_ticks = note[3]  # Get the duration information
+
+                # Convert timing from ticks to frames
+                timing_seconds = ticks_to_seconds(timing_ticks, bpm, division)
+                note[0] = int(seconds_to_frames(timing_seconds, fps))  # Convert to int for JSON serialization
+
+                # Convert duration from ticks to frames
+                duration_seconds = ticks_to_seconds(duration_ticks, bpm, division)
+                note[3] = int(seconds_to_frames(duration_seconds, fps))  # Convert to int for JSON serialization
+
+                note = tuple(note)  # Convert back to tuple and replace the original note
+                pattern['notes'][i] = note
+
+    # Convert ticks to frames for the 'timing' dictionary
+    for _, pattern_timing in timing.items():
+        for i, data in enumerate(pattern_timing):
+            start_time_ticks = data['start_time']  # Get the start_time information
+
+            # Convert start_time from ticks to frames
+            start_time_seconds = ticks_to_seconds(start_time_ticks, bpm, division)
+            data['start_time'] = int(seconds_to_frames(start_time_seconds, fps))  # Convert to int for JSON serialization
+
+            pattern_timing[i] = data
+
+    return details, timing
