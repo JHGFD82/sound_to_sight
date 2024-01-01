af4172c3974b204990ae6eff0059489ed509e71e
Jeff Heller <jeffheller@jhgfd.com>
2023-12-31 21:59:03 -0500

    Refactored, moved out embedded function.

--- sound_to_sight/clean_data.py
+++ sound_to_sight/clean_data.py
@@ -1,49 +1,41 @@
 import pandas as pd
 
 
-def clean_data(df):
-    """
+def prepare_for_extraction(df):
+    for column in ['time', 'num', 'note', 'velocity', 'extra1', 'extra2', 'player']:
+        df[column] = pd.to_numeric(df[column], errors='coerce').astype('Int32')
+    df['status'] = df['status'].str.strip()
+    df['instrument'] = df['instrument'].str.replace(r'\d+', '', regex=True).str.strip().str.lower()
+    return df
 
-    Cleans and processes the given DataFrame.
+
+def extract_relevant_data(df):
+    try:
+        division = df[df['status'] == "Header"]['velocity'].values[0]
+    except IndexError:
+        raise ValueError("Dataframe lacks 'Header' row!")
+    try:
+        notes_per_bar = df[df['status'] == "Time_signature"]['num'].values[0]
+    except IndexError:
+        raise ValueError("Dataframe lacks 'Time_signature' row!")
+    df = df[df['status'].isin(['Note_on_c', 'Note_off_c'])][
+        ['time', 'status', 'note', 'velocity', 'player', 'instrument']]
+    return df.reset_index(drop=True), division, notes_per_bar
+
+
+def clean_data(dataframe):
+    """
+    Clean and process DataFrame
 
     Parameters:
-        df (pandas.DataFrame): The DataFrame containing the data to be cleaned and processed.
+        dataframe (pandas.DataFrame): DataFrame that needs to be cleaned and processed
 
     Returns:
-        tuple: A tuple containing the cleaned DataFrame, the division value extracted from the DataFrame,
-            and the notes per bar value extracted from the DataFrame.
+        tuple: Tuple with cleaned DataFrame, division and notes_per_bar
 
     Raises:
-        ValueError: If the DataFrame is missing a 'Header' row or a 'Time_signature' row.
-
+        ValueError: If dataframe lacks 'Header' or 'Time_signature' row
     """
-    def prep_for_extraction(dataframe):
-        for column in ['time', 'num', 'note', 'velocity', 'extra1', 'extra2', 'player']:
-            dataframe[column] = pd.to_numeric(dataframe[column], errors='coerce').astype('Int32')
-        dataframe['status'] = dataframe['status'].str.strip()
-        dataframe['instrument'] = dataframe['instrument'].str.replace(r'\d+', '', regex=True).str.strip().str.lower()
-        return dataframe
-
-    def extract_relevant_data(dataframe):
-        try:
-            division_extract = dataframe[dataframe['status'] == "Header"]['velocity'].values[0]
-        except IndexError:
-            raise ValueError("The DataFrame is missing a 'Header' row!")
-
-        try:
-            notes_per_bar_extract = dataframe[dataframe['status'] == "Time_signature"]['num'].values[0]
-        except IndexError:
-            raise ValueError("The DataFrame is missing a 'Time_signature' row!")
-
-        valid_statuses = ['Note_on_c', 'Note_off_c']
-        dataframe_filtered = dataframe[dataframe['status'].isin(valid_statuses)][
-            ['time', 'status', 'note', 'velocity', 'player', 'instrument']]
-
-        dataframe_filtered = dataframe_filtered.reset_index(drop=True)
-
-        return dataframe_filtered, division_extract, notes_per_bar_extract
-
-    df = prep_for_extraction(df)
-    df, division, notes_per_bar = extract_relevant_data(df)
-
-    return df, division, notes_per_bar
+    dataframe = prepare_for_extraction(dataframe)
+    dataframe, division, notes_per_bar = extract_relevant_data(dataframe)
+    return dataframe, division, notes_per_bar
