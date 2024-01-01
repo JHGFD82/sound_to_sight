209eb3b287a3c14b93366c32c08731974c4aea40
Jeff Heller <jeffheller@jhgfd.com>
2023-12-31 21:54:05 -0500

    Renamed from "jsons" to "dictionaries"

--- sound_to_sight/__init__.py
+++ sound_to_sight/__init__.py
@@ -5,7 +5,7 @@ from .add_sections import add_sections
 from .measure_analysis import calculate_note_lengths
 from .measure_analysis import add_measure_time
 from .gather_instruments import gather_instruments
-from .assemble_dictionaries import create_jsons
+from .assemble_dictionaries import create_dictionaries
 from .extract_positions import extract_positions
 from .convert_timing import convert_timing
 from .export_jsons import export_jsons
