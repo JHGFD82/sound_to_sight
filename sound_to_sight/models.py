f8253f99eaa9d69aa70c652e2fba3d2772994415
Jeff Heller <jeffheller@jhgfd.com>
2024-01-08 08:52:45 -0500

    Better wording of attributes on objects.

--- sound_to_sight/models.py
+++ sound_to_sight/models.py
@@ -2,12 +2,14 @@ import mmh3
 
 
 class Note:
-    def __init__(self, start_time, length, note_value, velocity, instrument, x=None, y=None):
+    def __init__(self, start_time, measure_time, note_value, velocity, note_name, layout, x, y, length=None):
         self.start_time = start_time
+        self.measure_time = measure_time
         self.length = length
         self.note = note_value
         self.velocity = velocity
-        self.instrument = instrument
+        self.note_name = note_name
+        self.layout = layout
         # x and y will be initialized as None and can be updated later.
         self.x = x
         self.y = y
@@ -23,7 +25,7 @@ class Pattern:
         self.notes.append(note)
 
     def calculate_hash(self, notes):
-        pattern = [(note.start_time, note.note_value, note.velocity, note.length) for note in notes]
+        pattern = [(note.measure_time, note.note_value, note.velocity, note.length, note.layout) for note in notes]
         pattern_string = '_'.join('_'.join(map(str, tup)) for tup in pattern)
         return mmh3.hash(pattern_string)
 
