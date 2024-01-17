8f9624a3897a07d42ab57b3600fa38c984211f07
Jeff Heller <jeffheller@jhgfd.com>
2024-01-16 23:06:59 -0500

    Added processes for exporting JSON.

--- sound_to_sight/utils.py
+++ sound_to_sight/utils.py
@@ -21,3 +21,57 @@ def export_to_json(player_measures_dict, filename):
 
     with open(filename, 'w') as json_file:
         json.dump(section_dict, json_file, indent=4)
+
+
+def export_player_definitions(player_measures_dict, filename):
+    player_definitions = {}
+    for player in player_measures_dict.values():
+        player_number = player[0].player_number
+        instrument = player[0].instrument
+        layout = player[0].pattern.notes[0].layout
+
+        player_definitions[player_number] = {'instrument': instrument, 'layout': layout}
+
+    with open(filename, 'w') as json_file:
+        json.dump(player_definitions, json_file, indent=4)
+
+
+def export_pattern_definitions(player_measures_dict, filename):
+    pattern_definitions = {}
+    for player in player_measures_dict.values():
+        for player_measure in player:
+            pattern_hash = player_measure.pattern.hash
+            for note in player_measure.pattern.notes:
+                layout = note.layout
+                frame_start = note.frame_start
+                note_value = note.note_value
+                velocity = note.velocity
+                frame_duration = note.frame_duration
+                x = note.x
+                y = note.y
+
+                layout_dict = pattern_definitions.setdefault(layout, {})
+                hash_dict = layout_dict.setdefault(pattern_hash, []).append([
+                    frame_start, note_value, velocity, frame_duration, [x, y]
+                ])
+
+    pattern_definitions = {key: dict(sorted(inner_dict.items())) for key, inner_dict in pattern_definitions.items()}
+
+    with open(filename, 'w') as json_file:
+        json.dump(pattern_definitions, json_file)
+
+
+def calculate_fps(bpm, beats_per_measure, fps_min=24, fps_max=60):
+    # Calculate the duration of one measure in seconds
+    duration_of_one_measure = (60 * beats_per_measure) / bpm
+
+    # Iterate through the FPS range to find integer values
+    for fps in range(fps_max, fps_min - 1, -1):
+        frames_per_measure = fps * duration_of_one_measure
+
+        # Check if the frames per measure is an integer
+        if frames_per_measure.is_integer():
+            print(f"Adjusting frame rate of patterns to {fps} frames per second.")
+            return fps
+
+    return None
